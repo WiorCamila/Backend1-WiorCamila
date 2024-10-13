4a4a24b6175e7eaf88dfa848f0ac7905a2e8cdf9
Camila Wior Gdud <wiorcamila@gmail.com>
2024-10-13 17:53:54 -0300

    Primer commit

--- src/app.js
+++ src/app.js
@@ -7,7 +7,7 @@ const PORT=8080;
 const app=express();
 
 app.use(express.json());
-app.use(express.urlencoded({extended:true}));
+app.use(express.urlencoded({extended:true}))
 
 app.use("/api/products/", productsRouter)
 app.use("/api/carts/", cartsRouter)
